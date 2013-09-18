16c0c4e2291a6f2714bc758d49ec3699e94991b6
Jesse Falzone <jesse.falzone@gmail.com>
2013-09-18 18:40:01 -0400

    Scraped names and recipes, added more gems.

--- scrapers/scraper.rb
+++ scrapers/scraper.rb
@@ -1,1 +1,24 @@
-puts "Hello Dingus."
+require 'nokogiri'
+require 'rest-client'
+require 'mongoid'
+
+doc = Nokogiri::HTML(RestClient.get('http://www.esquire.com/drinks/drinks-full-list/'))
+names = doc.css(".result_content h2")
+names.each { |name| puts "#{name.text}\n"}
+puts names.length
+
+recipes = doc.css(".result_content ul")
+array = Array.new
+
+recipes.each do |recipe|
+  recipe_string = ""
+  items = recipe.css("li")
+  items.each do |item|
+    recipe_string += "#{item.text.strip}\n"
+  end
+  array << recipe_string
+  array << "--------------------------"
+end
+
+puts array
+
