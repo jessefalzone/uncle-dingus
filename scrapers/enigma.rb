83f021ebda63897e19934f99a6143c5c3035aa23
Jesse Falzone <jesse.falzone@gmail.com>
2013-09-23 18:50:43 -0400

    Added encode method.

--- scrapers/enigma.rb
+++ scrapers/enigma.rb
@@ -1,8 +1,28 @@
 class Enigma
+
   def initialize
+    @ingredients = ["whiskey","vermouth","van der hum","tequila",
+                         "sherry","rum","port","pastis","pernod",
+                         "orange curacao","guiness","grand marnier",
+                         "gin","dubonnet","drambuie","creme de menthe",
+                         "creme de cacao","cointreau","cognac","cherry liqueur",
+                         "champagne","campari","burgandy","brandy","benedictine",
+                         "beer","apricot liqueur","anisette","absinthe"]
+    @code_string = ""
   end
 
-  def encode
+  def encode(contents)
+    index = 0
+    @code_string == ""
+    @ingredients.each do |ingredient|
+      if ingredient == contents[index]
+        @code_string += "1"
+        index += 1
+      else
+        @code_string += "0"
+      end
+    end
+    puts @code_string
   end
 
   def decode
