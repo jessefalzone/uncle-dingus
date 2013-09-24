d7cb75a1a18e84f27e1e7e969a5748ab1471691d
Jesse Falzone <jesse.falzone@gmail.com>
2013-09-24 14:59:03 -0400

    Initial commit.

---
+++ topshelf/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TopShelf::Application.config.secret_key_base = '76fc99cdc703df5e1ce53d307dd1a628d69dd2d492778eef8bcd617087366f15d3ced4a81fc1a7c381124f3ad6c7a7b94579605e3c85cf0af61c58a20bb25e55'
---
+++ topshelf/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TopShelf::Application.config.session_store :cookie_store, key: '_TopShelf_session'
